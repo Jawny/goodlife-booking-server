3f8c631e53c21e410b122512028aacca8e016752
Johnny Liao <johnnyliao06@gmail.com>
2021-03-01 01:21:27 -0800

    added basic stripe integration. Still working on customer portal

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const Cryptr = require("cryptr");
+const Stripe = require("stripe");
 const { verifyLoginCredentials } = require("./utils/verifyLoginCredentials");
 require("dotenv").config();
 
 const cryptr = new Cryptr(process.env.CRYPTR_KEY);
+const stripe = Stripe(process.env.STRIPE_SECRET);
 
 mongoose.connect(
   `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.kqhf8.mongodb.net/goodlife?retryWrites=true&w=majority`,
@@ -16,7 +18,7 @@ mongoose.connect(
 const app = express();
 app.use(cors());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -73,5 +75,69 @@ app.post("/", async (req, res) => {
     console.log("sending:", false);
   }
 });
+
+app.post("/create-customer-portal-session", cors(), async (req, res) => {
+  const { payment_method, email } = req.body;
+  const customer = await stripe.customers.create({
+    payment_method,
+    email,
+    invoice_settings: {
+      default_payment_method: payment_method,
+    },
+  });
+  // Authenticate your user.
+  const session = await stripe.billingPortal.sessions.create({
+    customer: customer.id,
+    return_url: "http://localhost:3000/",
+  });
+
+  res.redirect(session.url);
+});
+
+app.post("/payment", cors(), async (req, res) => {
+  const { payment_method, email } = req.body;
+  try {
+    const customer = await stripe.customers.create({
+      payment_method,
+      email,
+      invoice_settings: {
+        default_payment_method: payment_method,
+      },
+    });
+
+    const subscription = await stripe.subscriptions.create({
+      customer: customer.id,
+      items: [{ plan: "price_1IQ2EwE6MqegVpJXrqqaXrTJ" }],
+      expand: ["latest_invoice.payment_intent"],
+    });
+
+    const status = subscription["latest_invoice"]["payment_intent"]["status"];
+    const clientSecret =
+      subscription["latest_invoice"]["payment_intent"]["client_secret"];
+
+    res.json({ clientSecret, status });
+  } catch (error) {
+    console.log("error", error);
+    res.json({ message: "payment failed", success: false });
+  }
+});
+
+// app.post("/payment", cors(), async (req, res) => {
+//   let { amount, id } = req.body;
+//   try {
+//     const payment = await stripe.paymentIntents.create({
+//       amount,
+//       currency: "CAD",
+//       description: "Goodlife Fitness Auto",
+//       payment_method: id,
+//       confirm: true,
+//     });
+//     console.log("payment", payment);
+//     res.json({ message: "Payment Successful", success: true });
+//   } catch (error) {
+//     console.log("error", error);
+//     res.json({ message: "payment failed", success: false });
+//   }
+// });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); // listens on this port
